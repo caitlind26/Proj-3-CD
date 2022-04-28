a9170ca19b43701c89acee6332d0ca6829ac1803
Caitlin D'Souza <cd394@njit.edu>
2022-04-28 11:47:29 -0400

    config changes

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 import os
 
-
 class Config(object):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     DEBUG = False
@@ -8,12 +7,12 @@ class Config(object):
     SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
     SESSION_COOKIE_SECURE = True
     BOOTSTRAP_BOOTSWATCH_THEME = 'Simplex'
-    db_dir = "database/db.sqlite"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.abspath(db_dir)
+    DB_DIR = os.getenv('DB_DIR','database')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR,'..', DB_DIR, "db.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
-
-
+    UPLOAD_FOLDER =  os.getenv('UPLOAD_FOLDER',  BASE_DIR + '/uploads')
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY','NOKEY')
+    LOG_DIR =  os.path.join(BASE_DIR, '../logs')
 class ProductionConfig(Config):
     pass
 
@@ -25,7 +24,6 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SESSION_COOKIE_SECURE = False
     DEBUG = True
-
