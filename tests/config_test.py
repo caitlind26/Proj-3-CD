6d85a15e4ab80c83e8e65b7cbdbed5fa80bb6cb3
Caitlin D'Souza <cd394@njit.edu>
2022-04-28 16:27:36 -0400

    .gitignore changes

--- tests/config_test.py
+++ tests/config_test.py
@@ -6,7 +6,6 @@ def test_development_config(application):
     application.config.from_object('app.config.DevelopmentConfig')
     assert application.config['DEBUG']
     assert not application.config['TESTING']
-    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
 
 
 def test_testing_config(application):
@@ -20,4 +19,3 @@ def test_production_config(application):
     application.config.from_object('app.config.ProductionConfig')
     assert not application.config['DEBUG']
     assert not application.config['TESTING']
-    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
